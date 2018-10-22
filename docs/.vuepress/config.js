ef3501c00749ca0fb5f7f31af77753ad03b4f195
nmsn <136696700@qq.com>
2018-10-22 22:11:23 +0800

    百度统计 谷歌分析

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: 'Somebody has to win, so why not be me?',
   head: [
     ['link', { rel: 'icon', href: `/favicon.ico` }],
+    ['script', {}, baiduLoad()],
   ],
   themeConfig: {
     // sidebar: 'auto',
@@ -26,6 +27,7 @@ module.exports = {
     // 自定义项目仓库链接文字
     // 默认根据 `themeConfig.repo` 中的 URL 来自动匹配是 "GitHub"/"GitLab"/"Bitbucket" 中的哪个，如果不设置时是 "Source"。
     repoLabel: 'Github',
+    ga: 'UA-127895888-1',
   }
 }
 
@@ -39,4 +41,15 @@ function genSidebarConfig(title) {
       'Console',
     ]
   }]
+}
+
+function baiduLoad() {
+  return `
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?6594ba1364804631f0a8fd4452766fed";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();`
 }
--- docs/README.md
+++ docs/README.md
@@ -12,4 +12,3 @@ actionLink: /zh/guide/
 #   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
 footer: MIT Licensed | NMSN 2018
 ---
-<Counter/>
