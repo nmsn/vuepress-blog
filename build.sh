5b53077351e437575d496be39fdfc71392ee72f0
nmsn <136696700@qq.com>
2018-11-08 23:23:25 +0800

    ci(*): build blog script

---
+++ build.sh
@@ -0,0 +1,15 @@
+# 构建
+yarn run docs:build
+
+# 进入生成的构建文件夹
+cd docs/.vuepress/dist
+
+# 如果你是要部署到自定义域名
+echo 'nmsn.site' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://<USERNAME>.github.io
+git push -f git@github.com:nmsn/nmsn.github.io.git master
