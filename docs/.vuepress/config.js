179151b68f069fc4809f59d6ae3662bda47613bb
shangyuan.ning <shangyuan.ning@manaowan.com>
2018-12-06 11:56:44 +0800

    refactor(config): 修改目录结构 调整配置代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,46 +8,45 @@ module.exports = {
     ['script', {}, mountGoogleAnalytics()],
   ],
   themeConfig: {
-    // sidebar: 'auto',
     nav: [
       { text: '主页', link: '/' },
       { text: '技术总结', link: '/tech/' },
-      { text: '笔记', link: '/notes/' },
+      { text: '摘录', link: '/excerpt/' },
+      { text: '读书笔记', link: '/readingNotes/' },
       { text: '计划', link: '/plan/' },
     ],
     lastUpdated: 'Last Updated',
     sidebar: {
-      '/tech/': genSidebarConfig('技术总结'),
+      '/tech/': [
+          {
+          title: '技术总结',
+          collapsable: false,
+          children: [
+            // '',
+            'Markdown',
+            'Console',
+            'Git',
+            'RegEx',
+            'MongoDB',
+          ]
+        },
+      ],
+      '/readingNotes/': [
+        {
+        title: '读书笔记',
+        collapsable: false,
+        children: [
+          // '',
+        ]
+      },
+    ],
     },
-    // sidebar: {
-    //   '/tech/':[
-    //     'Markdown',
-    //     'test2'
-    //     // 'README',
-    //   ],
-    // },
     repo: 'nmsn',
     // 自定义项目仓库链接文字
     // 默认根据 `themeConfig.repo` 中的 URL 来自动匹配是 "GitHub"/"GitLab"/"Bitbucket" 中的哪个，如果不设置时是 "Source"。
     repoLabel: 'Github',
     ga: 'UA-127895888-1',
   }
-}
-
-function genSidebarConfig(title) {
-  return [{
-    title,
-    collapsable: false,
-    children: [
-      '',
-      'Markdown',
-      'Console',
-      'Git',
-      'RegEx',
-      'MongoDB',
-      'addEventListener',
-    ]
-  }]
 }
 
 function mountBaiduAnalytics() {
