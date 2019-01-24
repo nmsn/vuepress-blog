5699595b48b6bca6e5dcada3eb5fc1c5f51c3841
shangyuan.ning <shangyuan.ning@manaowan.com>
2019-01-24 11:40:22 +0800

    refactor(gitalk): change repo

--- docs/.vuepress/components/Gitalk.vue
+++ docs/.vuepress/components/Gitalk.vue
@@ -11,7 +11,7 @@ export default {
     const gitalk = new Gitalk({
       clientID: 'b7b20f96965dba669ef8',
       clientSecret: '5781ec81ba8319c01fabc945a079e3a487701682',
-      repo: 'nmsn.github.io',
+      repo: 'blog-comments',
       owner: 'nmsn',
       admin: ['nmsn'],
       id: location.pathname,      // Ensure uniqueness and length less than 50
